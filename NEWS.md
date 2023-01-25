f40b0989f8605a79f792299414cfa0465d722faf
Amanda Mejia <mandy.mejia@gmail.com>
2023-01-25 15:00:02 +0000

    version 0.12.1

--- NEWS.md
+++ NEWS.md
@@ -1,17 +1,8 @@
-# 0.11.1
+# 0.12.1
 
-* Remove `fusedPCA`
-* Remove unused dependencies
-
-# 0.10.0
-
-* General package cleanup
-
-# 0.9.0
-
-* Add detrending
-* Add grayplot
-* Switch to `fastICA`
+* Use `fMRItools` where possible and remove duplicated functions from here.
+* Remove AppVeyor
+* Remove fusedPCA (glmgen is not on CRAN, and C++ has some problems)
 
 # 0.8.0
 
@@ -20,7 +11,6 @@
 * Update documentation
 * Some other interface changes
 * Remove robust distance-related stuff (for now)
-
 # 0.7.0
 
 * Renamed the package from `clever` to `fMRIscrub`
--- tests/testthat.R
+++ tests/testthat.R
@@ -3,18 +3,14 @@
 my_wb <- "../../workbench"
 
 library(testthat)
+library(ciftiTools)
+if (interactive()) { ciftiTools.setOption("wb_path", my_wb) }
+library(fMRItools)
 
-need_pkg <- c("ciftiTools", "fMRIscrub", "ggplot2", "cowplot", "fastICA")
-have_pkgs <- vapply(need_pkg, function(q){requireNamespace(q, quietly=TRUE)}, FALSE)
+library(fMRIscrub)
 
-if (all(have_pkgs)) {
-  library(ciftiTools)
-  if (interactive()) { ciftiTools.setOption("wb_path", my_wb) }
+library(ggplot2)
+library(cowplot)
+library(fastICA)
 
-  library(fMRIscrub)
-  library(ggplot2)
-  library(cowplot)
-  library(fastICA)
-
-  test_check("fMRIscrub")
-}
+test_check("fMRIscrub")
